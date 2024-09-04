2af06c64db71d9f18eb8b13081a48e8ccec5de55
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-09-04 12:44:53 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,39 @@
 function mergesort(array) {
+    let n = array.length;
+
+    // Start the subarrays of 1 and double with each iteration 
+    for(let length = 1, length < n; length *=2){
+        for(let left =0; left < n - 1; left += 2 * length) {
+            let mid = Math.min(left + length - 1, n - 1); 
+            let right = Math.min(left + 2 * length - 1, n-1); 
+            merge(array, left, mid, right);
+        }
+    }
+    
     return array;
+}
+
+function merge(array, left, mid, right) {
+    let start = left; 
+    let end = mid + 1; 
+
+    while (start <= mid && right <= right) {
+        if (array[start] <= array[end]) {
+            start+++; 
+        } else {
+            let value = array[end]; 
+            let index = end; 
+
+            while (index !== start) {
+                array[index] = array[index - 1]; 
+                index---; 
+            }
+
+            array[start] = value; 
+
+            start++;
+            mid++;
+            end++;
+        }
+    }
 }
